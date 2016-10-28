9696893babf8360b13889a4b40156fc8ad1e904f
jsculsp <328232234@qq.com>
2016-10-28 14:13:00 +0800

    'Chapter_5_exercises_finished'

--- rango/models.py
+++ rango/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
-    def __repr__(self):
+    def __str__(self):
         return self.name
 
 
