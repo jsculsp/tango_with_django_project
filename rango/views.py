c76b038922aedb27ddc49de2f8b53d93a8718750
Liu Shengpeng <328232234@qq.com>
2016-10-31 01:11:54 +0800

    add cookie function

--- rango/views.py
+++ rango/views.py
@@ -7,8 +7,24 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 from utils import log
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', 1))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    # last_visit_cookie[:-7] 切片操作用来去掉小数点后面的毫秒数
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).seconds > 10:
+        visits += 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
+
 
 
 def index(request):
@@ -21,8 +37,10 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list,
-                    'pages': page_list}
-    return render(request, 'rango/index.html', context_dict)
+                    'pages': page_list,}
+    response = render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 
 
 def about(request):
