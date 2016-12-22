b0ca39304123dd5f4eb771bb324758b225de11f5
jsculsp <328232234@qq.com>
2016-12-22 15:20:32 +0800

    save

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -1,8 +1,12 @@
-from django import template
-from rango.models import Category
 from datetime import datetime
+import string
+import random
+
+from django import template
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+
+from rango.models import Category
 
 register = template.Library()
 
@@ -13,9 +17,8 @@ def get_category_list(cat=None):
             'act_cat': cat}
 
 
-@register.simple_tag(takes_context=True)
+@register.simple_tag(takes_context=True, name='current_time')
 def current_time(context, format_string):
-
     # 这里用 mark_safe 函数，防止 datetime.now().strftime(format_string) 自动转义
     # https://docs.djangoproject.com/en/1.10/ref/utils/#django.utils.safestring.mark_safe 相关文档
     format_time = datetime.now().strftime(format_string)
@@ -27,3 +30,17 @@ def current_time(context, format_string):
     # return format_html('{}',
     #     mark_safe(datetime.now().strftime(format_string))
     # )
+
+
+@register.filter(name='times')
+def times(number):
+    return range(number)
+
+
+@register.simple_tag(name='random_color')
+def random_color():
+    choices = string.digits + string.ascii_uppercase[:6]
+    str1 = '#'
+    for i in range(6):
+        str1 += random.choice(choices)
+    return str1
