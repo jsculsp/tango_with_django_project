f3f46503ecbb78abee9fb6697861228f1be4efdf
jsculsp <328232234@qq.com>
2016-12-26 17:22:29 +0800

    save

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -53,3 +53,32 @@ def get_current_time_list(context):
     return {
         'user': context['user'],
     }
+
+
+@register.tag('format_time')
+def do_format_time(parse, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, date_to_be_formatted, format_string = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            '%r tag requires exactly two arguments' % token.contents.split()[0]
+        )
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+            '%r tag\'s argument should be in quotes' % tag_name
+        )
+    return FormatTimeNode(date_to_be_formatted, format_string[1: -1])
+
+
+class FormatTimeNode(template.Node):
+    def __init__(self, date_to_be_formatted, format_string):
+        self.date_to_be_formatted = template.Variable(date_to_be_formatted)
+        self.format_string = format_string
+
+    def render(self, context):
+        try:
+            actual_date = self.date_to_be_formatted.resolve(context)
+            return actual_date.strftime(self.format_string)
+        except template.VariableDoesNotExist:
+            return ''
