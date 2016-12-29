017a285aae25671331ae865e1d4a5193179c2393
jsculsp <328232234@qq.com>
2016-12-29 11:17:23 +0800

    save

--- rango/templatetags/jinja2.py
+++ rango/templatetags/jinja2.py
@@ -7,16 +7,7 @@ from django_jinja import library
 from django.utils.safestring import mark_safe
 from rango.models import Category
 
-
-@library.global_function(name='current_time')
-@jinja2.contextfunction
-def current_time(context, format_string):
-    # 这里用 mark_safe 函数，防止 datetime.now().strftime(format_string) 自动转义
-    # https://docs.djangoproject.com/en/1.10/ref/utils/#django.utils.safestring.mark_safe 相关文档
-    format_time = datetime.now().strftime(format_string)
-    data = '{} for {}'.format(format_time, context['user'])
-    return mark_safe(data)
-
+# Here are filters:
 
 @library.filter
 @jinja2.contextfilter
@@ -30,6 +21,18 @@ def replace(context, value, x, y):
 @library.filter(name='times')
 def times(number):
     return range(number)
+
+
+# Below are template tags:
+
+@library.global_function(name='current_time')
+@jinja2.contextfunction
+def current_time(context, format_string):
+    # 这里用 mark_safe 函数，防止 datetime.now().strftime(format_string) 自动转义
+    # https://docs.djangoproject.com/en/1.10/ref/utils/#django.utils.safestring.mark_safe 相关文档
+    format_time = datetime.now().strftime(format_string)
+    data = '{} for {}'.format(format_time, context['user'])
+    return mark_safe(data)
 
 
 @library.global_function(name='random_color')
@@ -55,3 +58,11 @@ def get_current_time_list(context):
 def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
             'act_cat': cat}
+
+
+@library.global_function
+def br_num(num):
+    s = ''
+    for i in range(num):
+        s += '<br> \n'
+    return mark_safe(s)
