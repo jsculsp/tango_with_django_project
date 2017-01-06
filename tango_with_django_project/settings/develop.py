fd13c2a0a9af887bce48ef7ee482c7a04cb4df36
jsculsp <328232234@qq.com>
2017-01-06 14:42:57 +0800

    save

--- tango_with_django_project/settings/develop.py
+++ tango_with_django_project/settings/develop.py
@@ -16,7 +16,7 @@ from jinja2.runtime import Undefined
 from django_jinja.builtins import DEFAULT_EXTENSIONS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 sys.path.insert(1, STATIC_DIR)
