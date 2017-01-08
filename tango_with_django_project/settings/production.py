63b0d77301f92e0143147c31edeae2034e07d65a
mu lin <mu lin>
2017-01-08 20:22:04 +0800

    save

--- tango_with_django_project/settings/production.py
+++ tango_with_django_project/settings/production.py
@@ -1,7 +1,9 @@
 from .develop import *
 
 DEBUG = True
+
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = 'https://static.linmu.date/static/'
 MEDIA_URL = 'https://static.linmu.date/media/'
