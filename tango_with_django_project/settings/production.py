cdee806b04cbfd25ba50aa35c235b31312819e02
mu lin <mu lin>
2017-01-08 19:49:48 +0800

    save

--- tango_with_django_project/settings/production.py
+++ tango_with_django_project/settings/production.py
@@ -4,3 +4,6 @@ DEBUG = True
 
 STATIC_URL = 'https://static.linmu.date/static/'
 MEDIA_URL = 'https://static.linmu.date/media/'
+
+DATABASES['default']['PASSWORD'] = 'root'
+DATABASES['default']['USER'] = 'root'
