7b3c15703d43e8bc73c22bba242715d461f2880f
jsculsp <328232234@qq.com>
2017-02-06 15:25:07 +0800

    save

--- aggregation_test/management/commands/command_test.py
+++ aggregation_test/management/commands/command_test.py
@@ -4,8 +4,66 @@ class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def add_arguments(self, parser):
+        # Positional arguments
         parser.add_argument('poll_id', nargs='+', type=int)
 
+        # Named (optional arguments)
+        parser.add_argument(
+            '--add',
+            action='store_true',
+            dest='add',
+            default=False,
+            help='add'
+        )
+        parser.add_argument(
+            '--substract',
+            action='store_true',
+            dest='substract',
+            default=False,
+            help='substract'
+        )
+        parser.add_argument(
+            '--multiply',
+            action='store_true',
+            dest='multiply',
+            default=False,
+            help='multiply'
+        )
+        parser.add_argument(
+            '--divide',
+            action='store_true',
+            dest='divide',
+            default=False,
+            help='divide'
+        )
+
+
     def handle(self, *args, **options):
-        for poll_id in options['poll_id']:
-            self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        s = ''
+        result = 0
+        tag = sum([options[i] for i in ['add', 'substract', 'multiply', 'divide']])
+        if options['add'] or not tag:
+            for poll_id in options['poll_id']:
+                s += '{} + '.format(poll_id)
+                result += poll_id
+            self.stdout.write(self.style.SUCCESS('{}= {}'.format(s[:-2], result)))
+        elif options['substract']:
+            result += options['poll_id'][0]
+            s = '{} - '.format(options['poll_id'][0])
+            for poll_id in options['poll_id'][1:]:
+                s += '{} - '.format(poll_id)
+                result -= poll_id
+            self.stdout.write(self.style.SUCCESS('{}= {}'.format(s[:-2], result)))
+        elif options['multiply']:
+            result = 1
+            for poll_id in options['poll_id']:
+                s += '{} × '.format(poll_id)
+                result *= poll_id
+            self.stdout.write(self.style.SUCCESS('{}= {}'.format(s[:-2], result)))
+        elif options['divide']:
+            result = options['poll_id'][0]
+            s = '{} ÷ '.format(options['poll_id'][0])
+            for poll_id in options['poll_id'][1:]:
+                s += '{} ÷ '.format(poll_id)
+                result /= poll_id
+            self.stdout.write(self.style.SUCCESS('{}= {}'.format(s[:-2], result)))
