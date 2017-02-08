b084b0fc45e3a407c9166dff1c29d98759d1016a
jsculsp <328232234@qq.com>
2017-02-08 17:24:56 +0800

    save

--- rango/apps.py
+++ rango/apps.py
@@ -1,13 +1,4 @@
 from django.apps import AppConfig
-# from django.db.models.signals import pre_save
-# from django.dispatch import receiver
-#
-# from utils import log
-
-
-class Count(object):
-    n = 0
-count = Count()
 
 
 class RangoConfig(AppConfig):
