d6a68bca8f3f69afe978231887e048c4135a4a12
mu lin <mu lin>
2017-02-18 23:09:35 +0800

    save

--- redis_demo/views.py
+++ redis_demo/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.core.cache import cache
 
 def index(request):
-    return render(request, 'redis_demo/index.html', context={})
+    if request.method == 'GET':
+        name = cache.get('name')
+        data = {
+            'name': name,
+        }
+        return render(request, 'redis_demo/index.html', data)
+    else:
+        name = request.POST['name']
+        cache.set('name', name)
+        return redirect(reverse('redis-demo_index'))
