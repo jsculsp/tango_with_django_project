7f272fc2a42b01045e537a599fca50162ac6647a
jsculsp <328232234@qq.com>
2017-02-22 15:50:28 +0800

    save

--- tango_with_django_project/settings/develop.py
+++ tango_with_django_project/settings/develop.py
@@ -25,6 +25,8 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 100},
+            "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
         }
     }
 }
