67fee9f2c827d856d5d1ffc0844f35edd1b04fb1
jsculsp <328232234@qq.com>
2017-06-22 11:02:27 +0800

    ave

--- rango/temp.py
+++ rango/temp.py
@@ -19,6 +19,7 @@ def verify_wechat_signature(view_func):
     @wraps(view_func, assigned=available_attrs(view_func))
     def wrapped_view(request, *args, **kwargs):
         plog(request.body)
+        log('type(request.body): ', type(request.body))
         content = """
             <xml>
                 <return_code><![CDATA[{0}]]></return_code>
